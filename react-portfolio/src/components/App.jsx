148c3d02b682fbab136a0621f89559155ac719b2
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-06-06 23:17:09 +1000

    dark mode comp added

--- react-portfolio/src/components/App.jsx
+++ react-portfolio/src/components/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import "../App.css";
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <div className="header">
         <h1>Portfolio start from here</h1>
         <p>This is the landing page for the portfolio</p>
+        hello world
       </div>
     </>
   );
