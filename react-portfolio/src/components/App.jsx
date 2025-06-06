8a3b8c8bcfb6c6b0edb81213db0551c805ef7119
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-06-06 23:33:15 +1000

    vercel speed insight package removed

--- react-portfolio/src/components/App.jsx
+++ react-portfolio/src/components/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "../App.css";
 import DarkMode from "./Darkmode";
 
 function App() {
+  <SpeedInsights />;
   return (
     <>
       <DarkMode />
