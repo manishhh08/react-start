d214ef2c4b95030e5d3585a810e3be173fb08c81
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-06-06 23:17:38 +1000

    merge commits with dark mode component

--- react-portfolio/src/components/App.jsx
+++ react-portfolio/src/components/App.jsx
@@ -2,15 +2,12 @@ import { useState } from "react";
 import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
 import "../App.css";
+import DarkMode from "./Darkmode";
 
 function App() {
   return (
     <>
-      <div className="header">
-        <h1>Portfolio start from here</h1>
-        <p>This is the landing page for the portfolio</p>
-        hello world
-      </div>
+      <DarkMode />
     </>
   );
 }
