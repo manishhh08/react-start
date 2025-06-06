b938b9cc89c51a7ddea9b94e28f365c1c7acc57c
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-06-06 23:40:29 +1000

    nav bar added

--- react-portfolio/src/components/App.jsx
+++ react-portfolio/src/components/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from "react";
 import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 import "../App.css";
 import DarkMode from "./Darkmode";
+import Navigation_menu from "./Navigation_menu";
 
 function App() {
-  <SpeedInsights />;
   return (
     <>
-      <DarkMode />
+      <Navigation_menu />
     </>
   );
 }
---
+++ react-portfolio/src/components/Navigation_menu.jsx
@@ -0,0 +1,51 @@
+function Navigation_menu() {
+  return (
+    <>
+      <header>
+        <div className="container">
+          <div className="logo">
+            <img src="assets/logo.png" alt="" />
+          </div>
+
+          <input type="checkbox" id="hamburgerMenu" />
+          <div className="menu">
+            <div>
+              <a href="#home">
+                <i className="fa-solid fa-house-chimney-crack"></i> HOME
+              </a>
+            </div>
+
+            <div>
+              <a href="#skills">
+                <i className="fa-solid fa-gear"></i> SKILLS
+              </a>
+            </div>
+
+            <div>
+              <a href="#projects">
+                <i className="fas fa-project-diagram"></i> PROJECTS
+              </a>
+            </div>
+
+            <div>
+              <a href="#about">
+                <i className="fa-solid fa-circle-info"></i> ABOUT
+              </a>
+            </div>
+
+            <div>
+              <a href="#contact">
+                {" "}
+                <i className="fa-solid fa-phone"></i> CONTACT
+              </a>
+            </div>
+          </div>
+          <label for="hamburgerMenu" test="hamburg">
+            <i className="fa-solid fa-bars"></i>
+          </label>
+        </div>
+      </header>
+    </>
+  );
+}
+export default Navigation_menu;
