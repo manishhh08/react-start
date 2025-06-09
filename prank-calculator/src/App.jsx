ea2ff6a2b94ccf8a13b629093de0a34665ba7af3
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-06-09 21:32:42 +1000

    calc ui created

--- prank-calculator/src/App.jsx
+++ prank-calculator/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <div class="wrapper">
+          <div class="output" id="ouput">
+            0.00
+          </div>
+          <div class="btn-body">
+            <div class="btn btn-c">C</div>
+            <div class="btn btn-b">B</div>
+            <div class="btn">/</div>
+            <div class="btn special-btn">*</div>
+            <div class="btn">7</div>
+            <div class="btn">8</div>
+            <div class="btn">9</div>
+            <div class="btn special-btn">-</div>
+            <div class="btn">4</div>
+            <div class="btn">5</div>
+            <div class="btn">6</div>
+            <div class="btn special-btn">+</div>
+            <div class="btn">1</div>
+            <div class="btn">2</div>
+            <div class="btn">3</div>
+            <div class="btn rowspan special-btn">=</div>
+            <div class="btn colspan">0</div>
+            <div class="btn">.</div>
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
