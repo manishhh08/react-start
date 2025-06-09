e982361e4265fcf34e16b4e877d840a353a2ed69
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-06-09 21:59:10 +1000

    app update

--- prank-calculator/src/App.jsx
+++ prank-calculator/src/App.jsx
@@ -2,29 +2,29 @@ function App() {
   return (
     <>
       <main>
-        <div class="wrapper">
-          <div class="output" id="ouput">
+        <div className="wrapper">
+          <div className="output" id="ouput">
             0.00
           </div>
-          <div class="btn-body">
-            <div class="btn btn-c">C</div>
-            <div class="btn btn-b">B</div>
-            <div class="btn">/</div>
-            <div class="btn special-btn">*</div>
-            <div class="btn">7</div>
-            <div class="btn">8</div>
-            <div class="btn">9</div>
-            <div class="btn special-btn">-</div>
-            <div class="btn">4</div>
-            <div class="btn">5</div>
-            <div class="btn">6</div>
-            <div class="btn special-btn">+</div>
-            <div class="btn">1</div>
-            <div class="btn">2</div>
-            <div class="btn">3</div>
-            <div class="btn rowspan special-btn">=</div>
-            <div class="btn colspan">0</div>
-            <div class="btn">.</div>
+          <div className="btn-body">
+            <div className="btn btn-c">C</div>
+            <div className="btn btn-b">B</div>
+            <div className="btn">/</div>
+            <div className="btn special-btn">*</div>
+            <div className="btn">7</div>
+            <div className="btn">8</div>
+            <div className="btn">9</div>
+            <div className="btn special-btn">-</div>
+            <div className="btn">4</div>
+            <div className="btn">5</div>
+            <div className="btn">6</div>
+            <div className="btn special-btn">+</div>
+            <div className="btn">1</div>
+            <div className="btn">2</div>
+            <div className="btn">3</div>
+            <div className="btn rowspan special-btn">=</div>
+            <div className="btn colspan">0</div>
+            <div className="btn">.</div>
           </div>
         </div>
       </main>
