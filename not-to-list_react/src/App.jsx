4331332af57c5942020d6567a695ec87f1f5e434
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-06-16 10:43:44 +1000

    not to do list react

--- not-to-list_react/src/App.jsx
+++ not-to-list_react/src/App.jsx
@@ -1,35 +1,130 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="wrapper">
+        <div className="container text-center">
+          {/* header row */}
+          <div className="row">
+            <div className="col p-5">
+              <h1>NOT TO DO LIST</h1>
+            </div>
+          </div>
+          {/* <!-- add task form row --> */}
+          <div className="row pt-5 pb-5 ps-4 pe-4 mt-5 mb-5 rounded-pill shadow-lg">
+            {/* <!-- task element --> */}
+            <div className="col-sm-12 col-md-6">
+              <input
+                type="text"
+                className="form-control"
+                id="task"
+                name="task"
+                placeholder="ADD TASK"
+              />
+            </div>
+            {/* <!-- hour element --> */}
+            <div className="col-sm-12 col-md-3 col-lg-2">
+              <input
+                type="number"
+                className="form-control"
+                id="hour"
+                name="hour"
+                placeholder="HOUR"
+              />
+            </div>
+            {/* <!-- type element --> */}
+            <div className="col-sm-12 col-md-3 col-lg-2">
+              <select className="form-select" name="type" id="type">
+                <option value="good">Good</option>
+                <option value="bad">Bad</option>
+              </select>
+            </div>
+
+            <div className="col-sm-12 col-md-2">
+              <button
+                type="button"
+                className="btn btn-primary"
+                id="addBtn"
+                // onClick="addTask()"
+              >
+                ADD TASK
+              </button>
+            </div>
+          </div>
+
+          {/* <!-- list row --> */}
+          <div className="row">
+            <div className="col-sm-12 col-md-6">
+              <h2>Good List</h2>
+              <hr />
+              <table className="table">
+                <tbody id="goodList"></tbody>
+              </table>
+              <div className="alert alert-success" role="alert">
+                You invested <span id="goodHour"></span> hour
+              </div>
+            </div>
+
+            <div className="col-sm-12 col-md-6">
+              <h2>Bad List</h2>
+              <hr />
+              <table className="table">
+                <tbody id="badList"></tbody>
+              </table>
+
+              <div className="alert alert-danger" role="alert">
+                You wasted <span id="badHour"></span> hour
+              </div>
+            </div>
+          </div>
+          {/* <!-- alert row --> */}
+          <div className="row">
+            <div className="col">
+              <div className="alert alert-info" role="alert">
+                Total Hours : <span id="totalHour"></span> hour
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="toast-container position-fixed bottom-0 end-0 p-3">
+        <div
+          id="liveToast"
+          className="toast"
+          role="alert"
+          aria-live="assertive"
+          aria-atomic="true"
+        >
+          <div className="toast-header">
+            <img src="ge" className="rounded me-2" alt="" />
+            <strong className="me-auto">TASK</strong>
+            <small>0 sec ago</small>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="toast"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="toast-body">
+            <span id="toast-msg"> </span>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <audio id="chime-audio">
+        <source src="assets/chime.mp3" />
+      </audio>
+      <audio id="swoosh-audio">
+        <source src="assets/swoosh.wav" />
+      </audio>
+      <audio id="delete-audio">
+        <source src="assets/delete.wav" />
+      </audio>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
